490369528e2a7eb39eca84543936fc157e66876a
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-12 23:30:07 +0800

    feat: Add sql from database

---
+++ Entity.Database/dbo/Tables/Products.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]          INT             NOT NULL,
+    [CreatedAt]   DATETIME        DEFAULT (getdate()) NOT NULL,
+    [Name]        NVARCHAR (50)   NOT NULL,
+    [Description] NVARCHAR (2000) NULL,
+    [Price]       MONEY           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+GO
+    CREATE NONCLUSTERED INDEX IX_Product_CreatedAt
+    ON [dbo].[Products] ([CreatedAt] ASC);
+
