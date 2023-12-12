43de58de86c058289cfef1fea7bc3636f9a31064
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-13 00:00:01 +0800

    feat: Add more indexes

--- Entity.Database/dbo/Tables/Products.sql
+++ Entity.Database/dbo/Tables/Products.sql
@@ -7,7 +7,20 @@
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
+
+
 GO
     CREATE NONCLUSTERED INDEX IX_Product_CreatedAt
     ON [dbo].[Products] ([CreatedAt] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Product_PriceMoreThanHundredThousand]
+    ON [dbo].[Products]([Price] ASC) WHERE ([Price]>=(100000));
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Product_PriceIncludeName]
+    ON [dbo].[Products]([Price] ASC)
+    INCLUDE([Name]);
 
