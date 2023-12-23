9dbf80f0dd4e72588295f39ca5d0a5a0d7a7c894
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-23 16:33:45 +0800

    feat: Add stored proc and view

---
+++ Entity.Database/dbo/Stored Procedures/sp_UserFilterByUsername.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[sp_UserFilterByUsername]
+	@Username NVARCHAR(50)
+AS
+BEGIN
+	SELECT * FROM dbo.Users
+	WHERE Username LIKE '%' + @Username + '%';
+END
+RETURN 0
---
+++ Entity.Database/dbo/Views/UserCredentials.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[UserCredentials]
+	AS SELECT
+		[u].[Id] UID,
+		[u].[Username],
+		[u].[CreatedAt] UCreatedAt,
+		[u].[UpdatedAt] UUpdatedAt,
+		[c].[Id] CID,
+		[c].[UserId],
+		[c].[HashedPassword],
+		[c].[Salt],
+		[c].[HashAlgorithm],
+		[c].[CreatedAt] CCreatedAt,
+		[c].[UpdatedAt] CUpdatedAt
+	FROM dbo.Users u
+	LEFT JOIN dbo.Credentials c
+	ON u.Id = c.UserId;
